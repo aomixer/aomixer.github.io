5a82e1375689496769731ec2a6a2c00741313c12
aomixer <53256207+aomixer@users.noreply.github.com>
2025-04-19 03:40:59 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
   mode: 'development',
 
-  // メインとなるJavaScriptファイル（エントリーポイント）
   entry: {
     script: `./_src/js/index.js`,
   },
@@ -15,16 +12,12 @@ module.exports = {
   module: {
     rules: [
       {
-        // 拡張子 .js の場合
         test: /\.js$/,
         use: [
           {
-            // Babel を利用する
             loader: 'babel-loader',
-            // Babel のオプションを指定する
             options: {
               presets: [
-                // プリセットを指定することで、ES2019 を ES5 に変換
                 '@babel/preset-env',
               ],
             },
@@ -41,7 +34,6 @@ module.exports = {
           filename: '[name][ext]',
           options: {
             encodeOptions: {
-              /* 各種libSquooshのオプション設定 */
               mozjpeg: {
                 quality: 90,
               },

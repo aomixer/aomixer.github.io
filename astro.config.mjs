b606566fd35e598b28b55affd66ec8b9288a250c
Nanako Mizuki <mizukinanako@gmail.com>
2025-04-21 13:18:50 +0900

    カスタムドメイン設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aomixer.github.io',
+	site: 'https://aomixer.com',
 	integrations: [mdx(), sitemap()],
 });
